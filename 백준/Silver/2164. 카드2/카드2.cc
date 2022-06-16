207a0a2168ae13e9e364530b52b741ea8317dd70
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2022-06-16 23:52:19 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,12 @@
+//2330
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    int num; cin>>num; queue<int> q;
+    for(int i=1;i<=num;i++) q.push(i);
+    while(q.size()!=1){
+        q.pop(); q.push(q.front()); q.pop();
+    }cout<<q.front();
+}
