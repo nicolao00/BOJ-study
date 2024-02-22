50ade4410d4c12d75886502938cffc376cc50a1c
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2024-02-22 13:22:52 +0900

    [Silver I] Title: 스타트와 링크, Time: 1664 ms, Memory: 56256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -11,22 +11,18 @@ for i in range(N):
     for j in range(i+1, N):
         ijSum[(i, j)] = board[i][j] + board[j][i]
 
-visit = {}
-answer = 1e9
 
+answer = 1e9
 def combSum(comb):
     result = 0
     for i in range(len(comb)):
         for j in range(i + 1, len(comb)):
             result += ijSum[(comb[i], comb[j])]
     return result
-
-for team1 in combinations(range(N), N//2):
-    if team1 in visit: continue
-    team1stet = combSum(team1)
-    team2 = [i for i in range(N) if i not in team1]
-    team2stet = combSum(team2)
-    visit[tuple(team2)] = team2stet
+combination = list(combinations(range(N), N//2))
+for i in range(len(combination)//2):
+    team1stet = combSum(combination[i])
+    team2stet = combSum(combination[len(combination)-i-1])
 
     answer = min(answer, abs(team1stet - team2stet))
 
