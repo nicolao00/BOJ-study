655a7442abcd426f54fc3ad6f1cc6c392679c76c
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2024-03-02 23:36:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1124 ms, Memory: 129632 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,26 @@
+# 850
+
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+N, M = map(int, input().split())
+
+board = [[0] * (N + 1)]
+for _ in range(N):
+    board.append([0] + list(map(int, input().split())))
+test = [list(map(int, input().split())) for _ in range(M)]
+stackSum = [[0] * (N + 1)]
+for _ in range(N):
+    stackSum.append([0] * (N + 1))
+
+# 각 칸까지의 누적합 구하기
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        stackSum[i][j] = stackSum[i-1][j] + stackSum[i][j-1] - stackSum[i-1][j-1] + board[i][j]
+
+for x1, y1, x2, y2 in test:
+    answer = stackSum[x2][y2] - stackSum[x2][y1-1] - stackSum[x1-1][y2] + stackSum[x1-1][y1-1]
+    print(str(answer)+'\n')
+
