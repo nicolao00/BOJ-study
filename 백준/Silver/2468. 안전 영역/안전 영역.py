e5c2347ccd91acf5e7d6755a314b7fa8e2637f45
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2024-03-03 21:18:00 +0900

    [Silver I] Title: 안전 영역, Time: 736 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,30 @@
+# 850
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+location = [list(map(int, input().split())) for _ in range(N)]
+
+def bfs(r, c):
+    dq = deque([(r, c)])
+    while dq:
+        r, c = dq.popleft()
+        for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < N and location[nr][nc] > height and not visit[nr][nc]:
+                dq.append((nr, nc))
+                visit[nr][nc] = True
+
+answer = 0
+for height in range(101):
+    result = 0
+    visit = [[False] * N for _ in range(N)]
+    for row in range(N):
+        for col in range(N):
+            if location[row][col] > height and not visit[row][col]:
+                result += 1
+                bfs(row, col)
+    if result == 0: break
+    answer = max(result, answer)
+print(answer)
