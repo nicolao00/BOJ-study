279f309a0f523578876cf481a6ecdb359f2d5ffa
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2025-02-15 02:49:14 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1610.51 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369#) 
 
 ### 성능 요약
 
-메모리: 11.7 MB, 시간: 2294.74 ms
+메모리: 11.7 MB, 시간: 1610.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 15일 02:37:52
+2025년 02월 15일 02:49:14
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,77 +1,55 @@
-# 548 - 640
-endIdxD = endIdxP = answer = 0
 
-
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
 def solution(cap, n, deliveries, pickups):
-    global endIdxD, endIdxP, answer
-    endIdxD = endIdxP = n - 1
-    answer = 0
-
-    def delivery():
-        global endIdxD, answer
-
-        # 상자가 있는 최초의 집
-        startIdx = -1
-        capacity = 0
-        # 최대 상자 개수를 채우거나 더이상 배달할 집이 없을 때 까지
-        while endIdxD >= 0 and capacity <= cap:
-            if startIdx == -1 and deliveries[endIdxD] != 0:
-                startIdx = endIdxD
-            # 최대 보유 상자 개수 이내
-            if capacity + deliveries[endIdxD] <= cap:
-                capacity += deliveries[endIdxD]
-                deliveries[endIdxD] = 0
-                endIdxD -= 1
-            # 최대 보유 상자 개수 초과
-            else:
-                deliveries[endIdxD] -= cap - capacity
-                break
-
-        return startIdx
-
-    def pickup(startIdx):
-        global endIdxP, answer
-
-        # 배달한 택배가 없을 때 (이동)
-        isMoved = True
-        if startIdx == -1:
-            isMoved = False
-        newStartIdx = -1
-
-        capacity = 0
-        while endIdxP >= 0 and capacity <= cap:
-            if newStartIdx == -1 and pickups[endIdxP] != 0:
-                newStartIdx = endIdxP
-            if capacity + pickups[endIdxP] <= cap:
-                capacity += pickups[endIdxP]
-                pickups[endIdxP] = 0
-                endIdxP -= 1
-            else:
-                pickups[endIdxP] -= cap - capacity
-                break
-
-        if not isMoved:
-            return newStartIdx
-
-        return newStartIdx
-
-    curLoc = -1
-    while endIdxD >= 0 or endIdxP >= 0:
-        deliverIdx = pickupIdx = -1
-        # 끝지점부터 배달
-        if endIdxD >= 0:
-            deliverIdx = delivery()
-
-        # 끝지점부터 수거
-        if endIdxP >= 0:
-            pickupIdx = pickup(deliverIdx)
-
-        farIdx = max(deliverIdx, pickupIdx)
-
-        if farIdx == -1:
-            curLoc = -1
-        else:
-            answer += 2 * (farIdx + 1)
-        curLoc = -1
-
-    return answer
+    dmax, pmax, ans = n-1, n-1, 0
+    if sum(deliveries) + sum(pickups) == 0 :
+        return 0
+    while dmax > -1 or pmax > -1:
+        ans += 2 * max(pmax+1, dmax+1)
+        dcar, pcar = cap, cap
+        while dmax > -1 and dcar > 0:
+            if deliveries[dmax] > dcar:
+                deliveries[dmax] -= dcar
+                dcar = 0
+            else :
+                dcar -= deliveries[dmax]
+                dmax -= 1
+            while dmax > -1 and deliveries[dmax] == 0 :
+                dmax -= 1    
+        while pmax > -1 and pcar > 0:
+            if pickups[pmax] > pcar:
+                pickups[pmax] -= pcar
+                pcar = 0
+            else :
+                pcar -= pickups[pmax]
+                pmax -= 1
+            while pmax > -1 and pickups[pmax] == 0 :
+                pmax -= 1
+    return ans
