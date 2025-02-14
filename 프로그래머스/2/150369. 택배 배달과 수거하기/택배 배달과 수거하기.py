00b75d6fad218a6a3e92b7cbd440440f7ad6bed5
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2025-02-15 02:54:05 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 373.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,55 +1,14 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
 def solution(cap, n, deliveries, pickups):
-    dmax, pmax, ans = n-1, n-1, 0
-    if sum(deliveries) + sum(pickups) == 0 :
-        return 0
-    while dmax > -1 or pmax > -1:
-        ans += 2 * max(pmax+1, dmax+1)
-        dcar, pcar = cap, cap
-        while dmax > -1 and dcar > 0:
-            if deliveries[dmax] > dcar:
-                deliveries[dmax] -= dcar
-                dcar = 0
-            else :
-                dcar -= deliveries[dmax]
-                dmax -= 1
-            while dmax > -1 and deliveries[dmax] == 0 :
-                dmax -= 1    
-        while pmax > -1 and pcar > 0:
-            if pickups[pmax] > pcar:
-                pickups[pmax] -= pcar
-                pcar = 0
-            else :
-                pcar -= pickups[pmax]
-                pmax -= 1
-            while pmax > -1 and pickups[pmax] == 0 :
-                pmax -= 1
-    return ans
+    answer = 0
+    dCap, pCap = 0, 0  # 배달 및 수거 용량
+
+    for i in range(n - 1, -1, -1):
+        dCap -= deliveries[i]
+        pCap -= pickups[i]
+
+        while dCap < 0 or pCap < 0:
+            answer += (i + 1) * 2
+            dCap += cap
+            pCap += cap
+
+    return answer
