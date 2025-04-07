3a0867465a34c42e6d9508818de470731f99ad2a
nicolao00 <74057742+nicolao00@users.noreply.github.com>
2025-04-07 16:11:59 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,34 +1,56 @@
-#329
 import sys
-from collections import deque
 input = sys.stdin.readline
+def parse_input():
 
-drdc = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-N, M = map(int, input().split())
-sRow, sCol, sD = map(int, input().split())
-board = [[1]*(M+2)]
-for _ in range(N):
-        board.append([1] + list(map(int, input().split())) + [1])
-board.append([1]*(M+2))
+    N, M = map(int, input().split())
+    r, c, d = map(int, input().split())
 
-answer = 1
-dq = deque([(sRow+1, sCol+1, sD)])
-board[sRow+1][sCol+1] = 2
-while dq:
-        r, c, d = dq.popleft()
-        flag = False
-        for dd in range(-1, -5, -1):
-                nd = d + dd
-                if nd < 0: nd = nd + 4
-                nr, nc = r + drdc[nd][0], c + drdc[nd][1]
-                if board[nr][nc] == 0:
-                        flag = True
-                        dq.append((nr, nc, nd))
-                        board[nr][nc] = 2
-                        answer += 1
-                        break
-        if not flag:
-                nr, nc = r - drdc[d][0], c - drdc[d][1]
-                if board[nr][nc] == 1: break
-                else: dq.append((nr, nc, d))
-print(answer)
+    grid = []
+    for _ in range(N):
+        grid.append(list(map(int, input().split())))
+
+    return N, M, r, c, d, grid
+
+
+def robot_vacuum(N, M, r, c, d, grid):
+    moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    cleaned_count = 0
+    cleaned_positions = set()
+
+    def can_move(x, y):
+        return 0 <= x < N and 0 <= y < M and grid[x][y] == 0
+
+    while True:
+        if (r, c) not in cleaned_positions:
+            cleaned_positions.add((r, c))
+            cleaned_count += 1
+
+        cleaned = False
+        for i in range(4):
+            d = (d + 3) % 4
+            nr, nc = r + moves[d][0], c + moves[d][1]
+
+            if can_move(nr, nc) and (nr, nc) not in cleaned_positions:
+                r, c = nr, nc
+                cleaned = True
+                break
+
+        if not cleaned:
+            # Move backward
+            nr, nc = r - moves[d][0], c - moves[d][1]
+            if not can_move(nr, nc):
+                break
+            r, c = nr, nc
+
+    return cleaned_count
+
+
+def main():
+    N, M, r, c, d, grid = parse_input()
+    result = robot_vacuum(N, M, r, c, d, grid)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
